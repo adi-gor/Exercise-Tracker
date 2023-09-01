8320ba7a7461dd214e5c07af51a24835bde8892e
Aditya Gor <adityagor13@gmail.com>
2023-09-01 17:29:05 +0530

    Fixed stopwatch bug

--- exercise-tracker/src/components/stopwatch.component.js
+++ exercise-tracker/src/components/stopwatch.component.js
@@ -22,8 +22,8 @@ const Stopwatch = (props) => {
   const hours = Math.floor(time / 360000);
 
   // Minutes calculation
-  const minutes = Math.floor((time % 360000) / 6000);
-
+  const minutes = Math.floor(time / 6000);
+  
   // Seconds calculation
   const seconds = Math.floor((time % 6000) / 100);
 
@@ -46,9 +46,8 @@ const Stopwatch = (props) => {
   return (
     <div className="stopwatch-container">
       <p className="stopwatch-time">
-        {hours}:{minutes.toString().padStart(2, "0")}:
-        {seconds.toString().padStart(2, "0")}:
-        {milliseconds.toString().padStart(2, "0")}
+        {minutes.toString().padStart(2, "0")}:
+        {seconds.toString().padStart(2, "0")}
       </p>
       <div className="stopwatch-buttons">
         <button className="stopwatch-button" onClick={startAndStop}>
